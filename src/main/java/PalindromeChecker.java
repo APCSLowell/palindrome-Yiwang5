c4ca1bb10642d95dce0f56f0b5db616957a7c3f9
Yiwang5 <yiwang5@s.sfusd.edu>
2025-01-27 10:05:03 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -55,8 +55,8 @@ public String reverse(String str)
     for(int i = 0; i < str.length(); i++){
     sNew = str.charAt(i) + sNew;
     return sNew;
+  }
 }
-
 public String onlyLetters(String sString){
   String word = "";
   for(int i = 0; i < sString.length(); i++){
